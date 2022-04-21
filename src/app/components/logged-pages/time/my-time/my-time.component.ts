edf31ca51fea16f4887e2f214f047494cbd65f58
Paulo <pauloh2012sul@gmail.compwd>
2022-04-21 03:18:12 -0300

    ajustes

--- src/app/components/logged-pages/time/my-time/my-time.component.ts
+++ src/app/components/logged-pages/time/my-time/my-time.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { CalendarOptions } from '@fullcalendar/angular';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CalendarOptions, CalendarApi, FullCalendarComponent } from '@fullcalendar/angular';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import ptLocale from '@fullcalendar/core/locales/pt';
 import { MatDialog } from '@angular/material/dialog';
+import moment from 'moment';
 
 @Component({
   selector: 'app-my-time',
@@ -11,9 +12,13 @@ import { MatDialog } from '@angular/material/dialog';
 })
 export class MyTimeComponent implements OnInit {
 
-  calendarPlugins = [dayGridPlugin]
+  @ViewChild('calendar') calendarComponent: FullCalendarComponent;
+
+  calendarPlugins = [dayGridPlugin];
 
   isWeekend: boolean = true;
+
+  currentMonth: string;
 
   calendarOptions: CalendarOptions = {
     locale: ptLocale,
@@ -23,16 +28,72 @@ export class MyTimeComponent implements OnInit {
     events: [
       { title: '3 hrs', date: '2022-04-05' },
       { title: '7 hrs', date: '2022-04-07' }
-    ]
+    ],
+    customButtons: {
+      next: {
+        text: "Próximo",
+        click: this.nextMonth.bind(this),
+      },
+      prev: {
+        text: "Anterior",
+        click: this.prevMonth.bind(this),
+      },
+
+      today: {
+        text: "Hoje",
+        click: this.today.bind(this),
+      }
+    },
   };
 
   constructor(private dialog: MatDialog) { }
 
   ngOnInit() {
+
   }
 
   handleDateClick(arg: any) {
     console.log('date click! ' + arg.dateStr)
+  }
+
+  nextMonth(): void {
+
+    let calendarApi = this.calendarComponent.getApi();
+
+    var date = moment(calendarApi.getDate(), "DD/MM/YYYY");
+
+    this.currentMonth = date.format("DD-MM-YYYY");
+
+    console.log(this.currentMonth);
+
+    calendarApi.next();
+
+  }
+
+  prevMonth(): void {
+
+    let calendarApi = this.calendarComponent.getApi();
+
+    var date = moment(calendarApi.getDate(), "DD/MM/YYYY");
+
+    this.currentMonth = date.format("DD-MM-YYYY");
+
+    console.log(this.currentMonth);
+
+    calendarApi.prev();
+  }
+
+  today(): void {
+
+    let calendarApi = this.calendarComponent.getApi();
+
+    var date = moment(calendarApi.getDate(), "DD/MM/YYYY");
+
+    this.currentMonth = date.format("DD-MM-YYYY");
+
+    console.log(this.currentMonth);
+
+    calendarApi.today();
   }
 
   close() {
