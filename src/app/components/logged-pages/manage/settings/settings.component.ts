0afd63e3a494273de028c33901de996230ac4f48
Paulo <pauloh2012sul@gmail.compwd>
2022-05-09 14:46:53 -0300

    delete user

--- src/app/components/logged-pages/manage/settings/settings.component.ts
+++ src/app/components/logged-pages/manage/settings/settings.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
+
+  //adiantamento
+  advanceRequestApproval: boolean = false;
+  emailsAdvanceNotify: string[] = [];
+  advanceAlertDays: number = 30;
+  maxOpenAdvance: number = 2;
+
+  //reembolso
+  refundRequestApproval: boolean = false;
+  maxOpenRefund: number = 3;
+  emailsRefundNotify: string[] = [];
+
+  //tempo
+  timeRequestApproval: boolean = true;
+  timeAlertDays: string[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.emailsAdvanceNotify.push('financeiro@empresa.com');
+    this.emailsRefundNotify.push('financeiro@empresa.com');
+
+    //time
+    this.timeAlertDays.push('Segunda, Terça, Quarta, Quinta, Sexta')
+
   }
 
 }
