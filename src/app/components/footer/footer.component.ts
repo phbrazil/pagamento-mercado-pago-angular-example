a3967b13bf2efb76d65d95db479e91d4a3117efa
Paulo <pauloh2012sul@gmail.compwd>
2022-05-24 03:52:00 -0300

    footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SubscribeService } from 'src/app/_services/subscribe.service';
 
 @Component({
   selector: 'app-footer',
@@ -8,9 +10,37 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  isLoading: boolean = false;
+
+  subscribeForm: FormGroup;
+
+  subscribed: boolean = false;
+
+  constructor(private fb: FormBuilder, private subscribeService: SubscribeService) { }
 
   ngOnInit(): void {
+
+    this.subscribeForm = this.fb.group({
+      email: ['', Validators.required],
+    });
+  }
+
+  submit(){
+
+    this.isLoading = true;
+
+    this.subscribeService.subscribe(this.subscribeForm.value.email).subscribe(res=>{
+
+      if(res){
+        this.subscribed = true;
+        this.subscribeForm.reset();
+      }
+      this.isLoading = false;
+
+    }, _err =>{
+      this.isLoading = false;
+    })
+
   }
 
 }
